9db4f7333eb5b8b2d1d9b2ed50249dff1c2b89ca
RichardNzembei <richardsonreuben78@gmail.com>
2025-06-02 22:33:51 +0300

    delivery service setup

--- Delivery-System/bin/main.dart
+++ Delivery-System/bin/main.dart
@@ -1,3 +1,13 @@
+import '../lib/DeliverySystem.dart';
+
 void main() {
-  print('hello world');
+  var order = Order(
+    customerName: 'Richie',
+    item: 'Pizza',
+    quantity: 1,
+    status: OrderStatus.placed,
+  );
+  order.trackOrder();
+  order.updateStatus(OrderStatus.prepaing);
+  order.trackOrder();
 }
